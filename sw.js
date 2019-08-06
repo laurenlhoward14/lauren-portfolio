bbc95478a02fc3f74a005a8e801afd47b76397eb
Jack Howard <jackhowaa@gmail.com>
2019-08-05 22:15:51 -0500

    Include new css for light mode below fold

--- sw.js
+++ sw.js
@@ -1,46 +1,46 @@
 importScripts('scripts/cache-polyfill.js');
-const LATEST_VERSION = 'cache20190804'
+const LATEST_VERSION = 'cache20190805'
 const CACHED_URLS = [
-  './',
-  'styles/styles.css',
-  'images/bio-175.png',
-  'images/bio-175.webp',
-  'favicon.ico',
-  'images/bio-192.png',
-  'manifest.json',
-  'images/outline-chat_bubble_outline-24px.svg'
+    './',
+    'styles/below-fold.css',
+    'styles/light-mode.css',
+    'images/bio-175.png',
+    'images/bio-175.webp',
+    'favicon.ico',
+    'images/bio-192.png',
+    'manifest.json'
 ]
 
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(LATEST_VERSION).then(function (cache) {
-      return cache.addAll(CACHED_URLS);
-    }).then(function (e) {
-      if (navigator.onLine) {
-        return self.skipWaiting();
-      }
-    })
-  )
-});
-
-self.addEventListener('fetch', function (event) {
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-self.addEventListener('activate', function (event) {
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (LATEST_VERSION !== cacheName) {
-            return caches.delete(cacheName);
-          }
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(LATEST_VERSION).then(function(cache) {
+            return cache.addAll(CACHED_URLS);
+        }).then(function(e) {
+            if (navigator.onLine) {
+                return self.skipWaiting();
+            }
         })
-      );
-    })
-  );
+    )
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (LATEST_VERSION !== cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
